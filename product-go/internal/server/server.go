44be8d6721bcfe07cc92cd0e569eb1f996b11aca
Alex Spicer <4531794+alex-spicer@users.noreply.github.com>
2025-02-23 17:47:40 -0600

    middleware for both go and sb3 (#2)

--- product-go/internal/server/server.go
+++ product-go/internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"product-go/internal/handler"
 	"product-go/internal/service"
 )
@@ -10,17 +11,31 @@ type Server struct {
 	router *gin.Engine
 }
 
+func myCustomMiddleware(c *gin.Context) {
+	// do something before request is sent
+	log.Println(c.Request.URL.Path)
+
+	c.Next()
+
+	// do something with response
+	status := c.Writer.Status()
+	log.Println(status)
+}
+
 func New() *Server {
 	router := gin.Default()
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	router.Use(gin.Recovery())
 
 	productService := service.NewProductService()
 	productHandler := handler.NewProductHandler(productService)
 
 	api := router.Group("/api")
 	{
-		products := api.Group("/products")
+		products := api.Group("/v1/products")
 		{
-			products.GET("", productHandler.GetAll)
+			products.GET("", myCustomMiddleware, productHandler.GetAll)
 			products.GET("/:id", productHandler.Get)
 			products.POST("", productHandler.Create)
 			products.PUT("/:id", productHandler.Update)
